adf3a0617c0aeef3529f7c90ccf70f9294b71719
amingsttc <amingst97@gmail.com>
2024-08-28 16:49:51 -0400

    add ecommerce shopify

---
+++ ECommerce/Shopify/IT.WebServices.ECommerce.Shopify.Services/Models/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace IT.WebServices.ECommerce.Shopify.Services.Models
+{
+    public class AppSettings
+    {
+        public string ShopDomain { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+    }
+}
