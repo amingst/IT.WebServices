3ed28950fc67ea8b7713577c3b2055a67c821a46
amingst <amingst97@gmail.com>
2025-05-10 17:47:22 -0400

    Add Discord Service Project, remove IntegrationInterface and IntegrationRecord

---
+++ Integrations/Discord/Services/DIExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IT.WebServices.Integrations.Discord.Services;
+public static class DIExtensions
+{
+    public static IServiceCollection AddDiscordIntegrationClasses(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static void MapDiscordIntegrationGrpcServices(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGrpcService<DiscordService>();
+    }
+}
---
+++ Integrations/Discord/Services/DiscordService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using IT.WebServices.Fragments.Authorization.Integrations.Discord;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace IT.WebServices.Integrations.Discord.Services
+{
+    [Authorize]
+    public class DiscordService : DiscordInterface.DiscordInterfaceBase
+    {
+        private readonly ILogger _logger;
+
+        public DiscordService(ILogger<DiscordService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<EnableDisableDiscordResponse> EnableDiscord(EnableDisableDiscordRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public override async Task<EnableDisableDiscordResponse> DisableDiscord(EnableDisableDiscordRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public override async Task<EditDiscordIntegrationResponse> EditDiscordIntegration(EditDiscordIntegrationRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+}
