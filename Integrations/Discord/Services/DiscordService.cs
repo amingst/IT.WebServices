cbc318a5b40b4303d6bd7df476bcb2a5a1906dcc
amingst <amingst97@gmail.com>
2025-05-10 19:03:32 -0400

    remove previous Discord Service Method Definitions

--- Integrations/Discord/Services/DiscordService.cs
+++ Integrations/Discord/Services/DiscordService.cs
@@ -1,9 +1,6 @@
-﻿using Grpc.Core;
-using IT.WebServices.Fragments.Authorization.Integrations.Discord;
+﻿using IT.WebServices.Fragments.Authorization.Integrations.Discord;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
 
 namespace IT.WebServices.Integrations.Discord.Services
 {
@@ -15,21 +12,6 @@ namespace IT.WebServices.Integrations.Discord.Services
         public DiscordService(ILogger<DiscordService> logger)
         {
             _logger = logger;
-        }
-
-        public override async Task<EnableDisableDiscordResponse> EnableDiscord(EnableDisableDiscordRequest request, ServerCallContext context)
-        {
-            throw new NotImplementedException("TODO");
-        }
-
-        public override async Task<EnableDisableDiscordResponse> DisableDiscord(EnableDisableDiscordRequest request, ServerCallContext context)
-        {
-            throw new NotImplementedException("TODO");
-        }
-
-        public override async Task<EditDiscordIntegrationResponse> EditDiscordIntegration(EditDiscordIntegrationRequest request, ServerCallContext context)
-        {
-            throw new NotImplementedException("TODO");
         }
     }
 }
