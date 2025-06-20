06f584f5f0c6c8b21fad580cb201e6d552baeed3
amingst <amingst97@gmail.com>
2025-06-20 15:07:29 -0400

    create events project
    create project for manual events service

---
+++ Authorization/Events/Manual/DIExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IT.WebServices.Authorization.Events.Manual.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DIExtensions
+    {
+        public static IServiceCollection AddManualEventsClasses(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static void MapManualEventsGrpcServices(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapGrpcService<ManualEventsService>();
+        }
+    }
+}
---
+++ Authorization/Events/Manual/Services/ManualEventsService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using IT.WebServices.Fragments.Authorization.Events;
+using Microsoft.Extensions.Logging;
+
+namespace IT.WebServices.Authorization.Events.Manual.Services
+{
+    public class ManualEventsService : EventInterface.EventInterfaceBase
+    {
+        private readonly ILogger<ManualEventsService> _logger;
+
+        public ManualEventsService(ILogger<ManualEventsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CreateEventResponse> CreateEvent(
+            CreateEventRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.CreateEvent(request, context);
+        }
+
+        public override Task<GetEventByIdResponse> GetEventById(
+            GetEventByIdRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.GetEventById(request, context);
+        }
+
+        public override Task<GetEventsResponse> GetEvents(
+            GetEventsRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.GetEvents(request, context);
+        }
+
+        public override Task<ModifyEventResponse> ModifyEvent(
+            ModifyEventRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.ModifyEvent(request, context);
+        }
+
+        public override Task<AnnounceEventResponse> AnnounceEvent(
+            AnnounceEventRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.AnnounceEvent(request, context);
+        }
+
+        public override Task<DeleteEventResponse> DeleteEvent(
+            DeleteEventRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.DeleteEvent(request, context);
+        }
+
+        public override Task<CreateTicketResponse> CreateTicket(
+            CreateTicketRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.CreateTicket(request, context);
+        }
+
+        public override Task<GetTicketResponse> GetTicket(
+            GetTicketRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.GetTicket(request, context);
+        }
+
+        public override Task<GetTicketsResposne> GetTickets(
+            GetTicketsRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.GetTickets(request, context);
+        }
+
+        public override Task<UnAnnounceEventResponse> UnAnnounceEvent(
+            UnAnnounceEventRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.UnAnnounceEvent(request, context);
+        }
+
+        public override Task<UnDeleteEventResponse> UnDeleteEvent(
+            UnDeleteEventRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.UnDeleteEvent(request, context);
+        }
+
+        public override Task<GetTicketsByEventResponse> GetTicketsByEvent(
+            GetTicketsByEventRequest request,
+            ServerCallContext context
+        )
+        {
+            return base.GetTicketsByEvent(request, context);
+        }
+    }
+}
