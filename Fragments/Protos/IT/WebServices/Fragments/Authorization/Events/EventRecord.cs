e99b264eede53bc497b7500713503d456f73fd73
amingst <amingst97@gmail.com>
2025-06-24 16:27:40 -0400

    Add Protos For Recurring Events and Fields For Publishing and Canceling an Event

--- Fragments/Protos/IT/WebServices/Fragments/Authorization/Events/EventRecord.cs
+++ Fragments/Protos/IT/WebServices/Fragments/Authorization/Events/EventRecord.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IT.WebServices.Fragments.Generic;
+using static IT.WebServices.Fragments.Authorization.Events.EventRecurrenceRule.Types;
 using pb = global::Google.Protobuf;
 
 namespace IT.WebServices.Fragments.Authorization.Events
@@ -15,6 +16,12 @@ namespace IT.WebServices.Fragments.Authorization.Events
             get => this.EventId.ToGuid();
             set => this.EventId = value.ToString();
         }
+
+        public bool IsRecurring =>
+            this.Public?.StartDate != null &&
+            this.Public?.EndDate != null &&
+            this.Public.Recurrence != null &&
+            this.Public.Recurrence.Frequency != RecurrenceFrequency.RepeatNone;
     }
 
     public sealed partial class EventTicketRecord : pb::IMessage<EventTicketRecord>
